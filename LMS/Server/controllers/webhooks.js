b80763c77369f3497a8fbbbe69ae26a05acf06ba
SRmohakal <royshourov71@gmail.com>
2025-12-09 08:56:13 +0600

    update

--- LMS/Server/controllers/webhooks.js
+++ LMS/Server/controllers/webhooks.js
@@ -1,55 +1,13 @@
-import { Webhook } from "svix";
-import User from "../models/User.js";
-
-
-//Api controller function to manage clerk user with database
-
-export const clerkWebhooks = async (req, res)=>{
+export const clerkWebhooks = async (req, res, next) => {
     try {
-        const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET)
-
-        await whook.verify(JSON.stringify(req.body), {
-            "svix-id": req.headers["svix-id"],
-            "svix-timestamp": req.headers["svix-timestamp"],
-            "svix-signature": req.headers["svix-signature"]
-        })
-
-        const {data, type} = req.body
-
-        switch (type) {
-            case 'user.created':{
-                const userData = {
-                    _id: data.id,
-                    email: data.email_addresses[0].email_address,
-                    name: data.first_name + " " + data.last_name,
-                    imageUrl: data.image_url,
-                }
-                await User.create(userData)
-                res.json({})
-                break;
-            }
-                
-            case 'user.updated': {
-                 const userData = {
-                    email: data.email_addresses[0].email_address,
-                    name: data.first_name + " " + data.last_name,
-                    imageUrl: data.image_url,
-                }
-                await User.findByIdAndUpdate(data.id, userData)
-                res.json({})
-                break;
-            }
-
-            case 'user.deleted' : {
-                await User.findByIdAndDelete(data.id)
-                res.json({})
-                break;
-            }
+        // Process Clerk webhook
+        console.log('Clerk webhook received')
         
-            default:
-                break;
-        }
-    } catch (error) {
-        res.json({success: false, message: error.message})
+        // TODO: Add your Clerk webhook processing logic here
+        // For now, just return success
+        res.status(200).json({ success: true, message: 'Webhook processed' })
+    } catch (err) {
+        console.error('Webhook processing error:', err)
+        res.status(500).json({ error: 'Webhook processing failed' })
     }
 }
